a3f3080a58e981c6618628fc83d29a2112d5ba93
mjtedder <mjtedder@gmail.com>
2018-06-14 16:42:24 -0400

    express routes functional

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
+//Dependencies
 let express = require("express");
 let bodyParser = require("body-parser");
 let path = require("path");
 
+//Sets up basic properties for our express server
 let app = express();
+
+//Sets initial port
 let PORT = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
+//Testing communication between front/back end.
+//app.get('/', function (req, res) {
+    //res.send('Hello World')
+  //})
 
+//Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(bodyParser.json())
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-})
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+require("/routes/apiroutes")(app);
+require("/routes/htmlroutes")(app);
 
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
