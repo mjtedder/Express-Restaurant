c42f360e09fe6d557e34773d32a33276ea284cc5
mjtedder <mjtedder@gmail.com>
2018-06-14 11:02:25 -0400

    code working. app is listening to server

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ let bodyParser = require("body-parser");
 let path = require("path");
 
 let app = express();
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    res.end(JSON.stringify(req.body, null, 2))
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
