cdaca28fa4640f8638565b972a94ecc8b218eb40
mjtedder <mjtedder@gmail.com>
2018-06-15 08:43:28 -0400

    added empty arrays to hold data

--- data/tableData.js
+++ data/tableData.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+let tableArray = [];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
